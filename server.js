1a1b19855cb061a89643651f5fffd4b674e8f532
Edward Kim <taesup63@gmail.com>
2019-03-07 14:22:13 -1000

    Updated to handle production enviroments

--- server.js
+++ server.js
@@ -1,28 +1,50 @@
 const express = require('express');
-const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
 const User = require('./database/models/User');
 
-dotenv.config();
+// data vars
 const PORT = process.env.PORT;
-const app = express();
+const SESSION_SECRET = process.env.SESSION_SECRET;
+const REDIS_HOSTNAME = process.env.REDIS_HOSTNAME;
 
-app.get('/', (req, res) => {
-  res.send('hello');
+if (!PORT) { console.log('No Port Found'); }
+if (!SESSION_SECRET) { console.log('No Session Secret Found'); }
+if (!REDIS_HOSTNAME) { console.log('No Redis Hostname Found'); }
+if (!PORT || !SESSION_SECRET || !REDIS_HOSTNAME) { return process.exit(1); }
+
+// setup server middleware
+const app = express();
+app.use(bodyParser.json({ extended: true }));
+
+// routes
+app.get('/api/smoke', (req, res) => {
+  res.json({ smoke: 'test' });
 });
 
-app.get('/users', (req, res) => {
-  return User.fetchAll()
-    .then((results) => {
-      if (results.length) {
-        results = results.toJSON();
-        return res.json(results);
-      }
-      else {
-        return res.status(500).send('Users not found');
-      }
+app.get('/api/users', (req, res) => {
+  return new User().fetchAll()
+    .then((users) => {
+      return res.json(users);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
+
+app.post('/api/users', (req, res) => {
+  const username = req.body.username;
+  return new User({ username }).save()
+    .then((user) => {
+      return res.json({ success: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
     });
 })
 
+// start server
 app.listen(PORT, () => {
-  console.log(`Server running on PORT: ${PORT}`);
+  console.log(`Server stated on port: ${PORT}`);
 });
